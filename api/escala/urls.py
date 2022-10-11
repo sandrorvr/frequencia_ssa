f51314c83d7132c3207aeeb48359b63d8588a98f
sandrorvr <sandrorvr@gmail.com>
2022-10-11 14:57:02 -0300

    add new views

--- api/escala/urls.py
+++ api/escala/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import EscalaAPIView
 
 urlpatterns = [
-    path('worker/<str:worker>', EscalaAPIView.as_view(), name='escala'),
+    path('/<str:worker>', EscalaAPIView.as_view(), name='escala'),
+    path('workers/<str:worker>', , name='escala'),
+    path('footHold/<str:worker>', EscalaAPIView.as_view(), name='escala'),
+
 ]
--- api/escala/views.py
+++ api/escala/views.py
@@ -7,8 +7,39 @@ from .serializers import EscalaSerializers, MachSerializers
 
 import numpy as np
 
-class EscalaAPIView(APIView):
+class matchWorker:
+    def __init__(self, worker):
+        self.worker = worker
+        self.sizeMatch = None
 
+    def get_date_road(self):
+        fullWorker = Escala.objects.filter(worker=self.worker)
+        date = []
+        road = []
+        for wk in fullWorker:
+            date.append(wk.date)
+            road.append(wk.road)
+        self.sizeMatch = len(date)
+        return (date, road)
+
+    @staticmethod
+    def flaten(self, vet):
+        vet_aux = []
+        for sublist in vet:
+            for item in sublist:
+                vet_aux.append(item)
+        return vet_aux
+
+    def get_match(self):
+        workersMach = []
+        dates, roads = self.get_date_road()
+        for i in range(self.sizeMatch):
+            filtered = Escala.objects.filter(date=dates[i], road=roads[i]).exclude(worker=self.worker)
+            serializer = EscalaSerializers(filtered, many=True)
+            workersMach.append(serializer.data)
+        return workersMach
+
+class EscalaAPIView(APIView):
     def get(self, request, worker):
         fullWorker = Escala.objects.filter(worker=worker)
         setDate = []
@@ -21,7 +52,7 @@ class EscalaAPIView(APIView):
         for i in range(len(setDate)):
             filtered = Escala.objects.filter(date=setDate[i], road=setRoad[i]).exclude(worker=worker)
             serializer = EscalaSerializers(filtered, many=True)
-            workersMach.append(serializer.data)        
+            workersMach.append(serializer.data)
 
         _workersMach = []
         for sublist in workersMach:
@@ -29,3 +60,6 @@ class EscalaAPIView(APIView):
                 _workersMach.append(item)
 
         return Response(_workersMach)
+
+class workers(APIView):
+    def get(self, request, worker):
